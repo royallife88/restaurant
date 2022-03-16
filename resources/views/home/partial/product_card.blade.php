92a9af0ef5ee978c079e450ea726a244dc19056a
royallife88 <devdeb@outlook.com>
2022-03-16 16:43:17 +0500

    product card square size

--- resources/views/home/partial/product_card.blade.php
+++ resources/views/home/partial/product_card.blade.php
@@ -1,21 +1,19 @@
-<a data-href="{{action('ProductController@show', $product->id)}}" class="product_card text-center md:w-1/4 xs:w-1/3 xl:p-16 lg:p-8 xs:p-1 sm:p-2">
-    <div class="flex-col mx-auto">
-        <div class="relative overflow-hidden w-full rounded-lg xl:72 lg:h-64 md:h-60 sm:h-48 xs:h-36 product_card">
-            <img src="{{!empty($product->getFirstMediaUrl('product')) ? $product->getFirstMediaUrl('product') : asset('uploads/'. session('logo'))}}"
-                class="border-2 border-dark mx-auto w-full h-full rounded-lg object-cover " alt="category-1">
-            <div
-                class="absolute w-full pb-2.5 pt-8 px-5 bottom-0 inset-x-0 text-white text-xs text-center leading-4 bg-gradient-to-t from-black">
-                <p class="text-tiny font-semibold text-white py-0">{{$product->name}}</p>
-                <p class="text-tiny font-semibold text-white py-0">{{$product->description}}</p>
-                <p class="text-tiny font-semibold text-white py-0">TL {{@num_format($product->sell_price - $product->discount_value)}}</p>
-            </div>
-            <div class="absolute bottom-0 mx-auto w-full">
-                <button data-product_id="{{$product->id}}" type="button"
-                    class="bg-white text-red hover:bg-red hover:text-white transition-all duration-500 w-12 h-12 rounded-full mb-16 opacity-0 cart_button">
-                    <i class=" fa fa-lg fa-cart-plus cart_icon"></i>
-                </button>
-            </div>
+<a href="{{ action('ProductController@show', $product->id) }}"
+    class="w-full h-0 shadow-lg pb-full rounded-xl bg-yellow-300 bg-center bg-no-repeat bg-cover relative mb:mb-8 xs:mb-16 border-2 border-dark product_card"
+    style="background-image: url('{{ !empty($product->getFirstMediaUrl('product'))? $product->getFirstMediaUrl('product'): asset('uploads/' . session('logo')) }}')">
+    <div
+        class="absolute w-full pb-2.5 pt-8 px-5 bottom-0 inset-x-0 text-white text-xs text-center leading-4 bg-gradient-to-t from-black">
+        <p class="text-tiny font-semibold text-white py-0">{{ $product->name }}</p>
+        <p class="text-tiny font-semibold text-white py-0">{{ $product->description }}</p>
+        <p class="text-tiny font-semibold text-white py-0">TL
+            {{ @num_format($product->sell_price - $product->discount_value) }}</p>
+    </div>
+    <div class="flex  w-full text-center">
+        <div class="absolute bottom-0 mx-auto w-full">
+            <button data-product_id="{{ $product->id }}" type="button"
+                class="bg-white text-red hover:bg-red hover:text-white transition-all duration-500 w-12 h-12 rounded-full mb-16 opacity-0 cart_button">
+                <i class=" fa fa-lg fa-cart-plus cart_icon"></i>
+            </button>
         </div>
-
     </div>
 </a>
--- resources/views/product/index.blade.php
+++ resources/views/product/index.blade.php
@@ -4,13 +4,25 @@
 @include('layouts.partials.category-header')
 
 <div class="container mx-auto mt-14">
-    @foreach ($products->chunk(4) as $product_chunk)
+
+    <div class="w-full mx-auto p-4">
+        <div class="grid xs:grid-cols-3 md:grid-cols-4 xs:gap-2 md:gap-16 md:mt-12 xs:mt-6">
+            @foreach ($products as $product)
+            @include('home.partial.product_card', ['product' => $product])
+            @endforeach
+        </div>
+    </div>
+
+
+    {{-- @foreach ($products->chunk(4) as $product_chunk)
     <div class="flex flex-row flex-wrap justify-center">
         @foreach ($product_chunk as $product)
         @include('home.partial.product_card', ['product' => $product])
         @endforeach
     </div>
-    @endforeach
+    @endforeach --}}
+
+
     @include('layouts.partials.cart-row')
 </div>
 

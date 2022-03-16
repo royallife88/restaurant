6bc10461f02273f42716de5a4e2ab79e051e1cd5
royallife88 <devdeb@outlook.com>
2022-03-16 17:27:00 +0500

    logo adjustment

--- resources/views/layouts/partials/main-header.blade.php
+++ resources/views/layouts/partials/main-header.blade.php
@@ -1,17 +1,20 @@
 <div class="w-full flex flex-row bg-center bg-no-repeat bg-cover text-center items-center"
-    style="height: 542px; background-image: url('@if(!empty(session('home_background_image'))){{ asset('uploads/' . session('home_background_image')) }}@else{{ asset('images/default-home-bg.png') }}@endif')">
+    style="height: 542px; background-image: url('@if (!empty(session('home_background_image'))) {{ asset('uploads/' . session('home_background_image')) }}@else{{ asset('images/default-home-bg.png') }} @endif')">
     <div class="w-full">
         <div class="flex flex-row">
             <div class="flex-3 text-white text-4xl font-bold text-left w-48">
                 @include('layouts.partials.language_dropdown')
             </div>
-            <div class="flex-1  mt-8">
-                <img src="{{ asset('uploads/' . session('logo')) }}" alt="logo"
-                    class="mx-auto md:w-56 md:h-56 md:w-40 md:h-40">
+            <div class="flex-1">
             </div>
             <div class="flex-3 w-48 items-end">
                 @include('layouts.partials.social_icons')
             </div>
+        </div>
+        <div class="flex flex-row">
+            <img src="{{ asset('uploads/' . session('logo')) }}" alt="logo"
+                class="mx-auto md:w-56 md:h-56 xs:w-40 xs:h-40">
+
         </div>
 
     </div>

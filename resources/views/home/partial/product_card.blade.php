acebb6f262715411f84d48e4dd4eee6f39d6cf95
royallife88 <devdeb@outlook.com>
2022-03-19 10:55:38 +0500

    product card border radius changes

--- resources/views/home/partial/product_card.blade.php
+++ resources/views/home/partial/product_card.blade.php
@@ -2,7 +2,7 @@
     class="w-full h-0 shadow-lg pb-full rounded-xl bg-center bg-no-repeat bg-cover relative mb:mb-8 xs:mb-16 border-2 border-dark product_card"
     style="background-image: url('{{ !empty($product->getFirstMediaUrl('product'))? $product->getFirstMediaUrl('product'): asset('uploads/' . session('logo')) }}')">
     <div
-        class="absolute w-full pb-2.5 pt-8 px-5 bottom-0 inset-x-0 text-white text-xs text-center leading-4 bg-gradient-to-t from-black">
+        class="absolute w-full pb-2.5 pt-8 px-5 bottom-0 inset-x-0 text-white text-xs text-center leading-4 bg-gradient-to-t from-black rounded-xl">
         <p class="md:text-sm xs:text-tiny font-semibold text-white py-0">{{ $product->name }}</p>
         <p class="md:text-sm xs:text-tiny font-semibold text-white py-0">{{ $product->description }}</p>
         <p class="md:text-sm xs:text-tiny font-semibold text-white py-0">TL

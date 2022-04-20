bc25fe913356595c5bcf77f04cd37df8458b8499
royallife88 <devdeb@outlook.com>
2022-04-20 19:33:32 +0500

    ui changes 3

--- resources/views/layouts/partials/product-header.blade.php
+++ resources/views/layouts/partials/product-header.blade.php
@@ -1,15 +1,20 @@
 <div class="w-full flex flex-row bg-no-repeat bg-center bg-cover text-center items-center"
-    style="height: 200px; background-image: url('@if(!empty(session('breadcrumb_background_image'))){{ asset('uploads/' . session('breadcrumb_background_image')) }}@else{{ asset('images/default-breadcrumb-bg.png') }}@endif');">
+    style="height: 200px; background-image: url('@if (!empty(session('breadcrumb_background_image'))) {{ asset('uploads/' . session('breadcrumb_background_image')) }}@else{{ asset('images/default-breadcrumb-bg.png') }} @endif');">
     <div class="w-full">
         <div class="flex flex-row">
             <div class="flex-3 text-white text-4xl font-bold text-left">
                 @include('layouts.partials.language_dropdown')
             </div>
-            <div class="flex-1 mt-8 text-center">
-                <p class="font-bold text-4xl text-white">{{ $product->name }}</p>
+            <div class="flex-1">
+
             </div>
             <div class="flex-3 w-48 items-end">
                 @include('layouts.partials.social_icons')
+            </div>
+        </div>
+        <div class="flex flex-row mt-2 text-center">
+            <div class="flex-1">
+                <p class="font-bold md:text-4xl xs:text-xl text-white text-center">{{ $product->name }}</p>
             </div>
         </div>
 
